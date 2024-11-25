d200a8787fb6513e01bdadd3f148ec55c0a17709
JoelCott <2406363@leedstrinity.ac.uk>
2024-11-25 11:49:23 +0000

    Done

--- MongoDB_ToolsPage/ConnectionTest.py
+++ MongoDB_ToolsPage/ConnectionTest.py
@@ -2,19 +2,23 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://2406363:Joel1234@dbtest.vqz9n.mongodb.net/toolsDB?retryWrites=true&w=majority")
 db = client.toolsDB
-comments_collection = db.comments
+comments_collection = db.users
 
-toolID = int(input("Enter tool ID: "))
-user = input("Enter user name: ")
-rating = int(input("Enter tool rating: "))
-comment = input("Enter a comment about the tool: ")
+FirstName = input("Enter First name: ")
+LastName = input("Enter Last name: ")
+Email = input("Enter user name: ")
+PhoneNo = int(input("Enter your phone Number: "))
+Picture = input("Enter profile pic: ")
+Password = input("Enter Password:  ")
 
 # Test insertion
 comment_data = {
-    "tool_id": toolID, # "64abcd1234ef567890123456" # Replace with a valid ObjectId string from your tools collection
-    "user": user, # "Test User"
-    "rating": rating, # 5
-    "comment": comment # "This is a test comment"
+    "fname": FirstName, # "64abcd1234ef567890123456" # Replace with a valid ObjectId string from your tools collection
+    "lname": LastName,
+    "email": Email, # "Test User"
+    "phone": PhoneNo, # 5
+    "picture": Picture,
+    "password": Password # "This is a test comment"
 }
 
 # Insert a comment to create the collection if it doesn’t exist
